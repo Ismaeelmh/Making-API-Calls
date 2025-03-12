63bdd9aebd2f409f7f964eb9b145e17f851d51ed
Ismaeel <ismaeelmh75@gmail.com>
2025-03-12 22:37:44 +0100

    add reponse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 
+MY_LAT = 51.507351
+MY_LONG = -0.127758
+
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 
@@ -11,3 +14,13 @@ latitude = data["iss_position"]["latitude"]
 iss_position = (longitude, latitude)
 
 print(iss_position)
+
+parameters ={
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+}
+
+
+reponse = requests.get("https:///api.sunrise-sunset.org/json", params=parameters)
+reponse.raise_for_status()
+data = response.json()
