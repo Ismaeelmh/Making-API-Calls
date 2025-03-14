2859d2f083e84f5eab0d60e3445937196f2991b6
Ismaeel <ismaeelmh75@gmail.com>
2025-03-15 00:50:13 +0100

    set the code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import datetime
 import requests
+from datetime import datetime
 
 MY_LAT = 51.507351
 MY_LONG = -0.127758
@@ -18,9 +20,18 @@ print(iss_position)
 parameters ={
     "lat": MY_LAT,
     "lng": MY_LONG,
+    "formatted": 0,
 }
 
 
-reponse = requests.get("https:///api.sunrise-sunset.org/json", params=parameters)
-reponse.raise_for_status()
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
 data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+print(time_now.hour)
